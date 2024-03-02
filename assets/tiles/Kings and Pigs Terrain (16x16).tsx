49d3b1426c0b04ad996ffacdd9bdea9eeeb77337
Abdul Arif <abdul.arif0705@gmail.com>
2024-03-01 20:02:24 -0500

    Add new 16x16 tilesets and remove old tileset files

--- assets/tiles/Kings and Pigs Terrain (16x16).tsx
+++ assets/tiles/Kings and Pigs Terrain (16x16).tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Kings and Pigs" tilewidth="32" tileheight="32" tilecount="247" columns="19">
+<tileset version="1.10" tiledversion="1.10.2" name="Kings and Pigs Terrain (16x16)" tilewidth="16" tileheight="16" tilecount="988" columns="38">
  <image source="../images/Terrain/Kings and Pigs Terrain (32x32).png" width="608" height="416"/>
 </tileset>
---
+++ assets/tiles/Treasure Hunters Terrain (16x16).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Treasure Hunters Terrain (16x16)" tilewidth="16" tileheight="16" tilecount="340" columns="34">
+ <image source="../images/Terrain/Treasure Hunters Terrain (32x32).png" width="544" height="160"/>
+</tileset>
