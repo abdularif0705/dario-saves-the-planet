a700f65b64f01f50ab105a763ff528ade6c86a24
Abdul Arif <abdul.arif0705@gmail.com>
2024-03-01 19:13:34 -0500

    Add new tilesets for terrain

---
+++ assets/tiles/Kings and Pigs.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Kings and Pigs" tilewidth="32" tileheight="32" tilecount="247" columns="19">
+ <image source="../images/Terrain/Kings and Pigs Terrain (32x32).png" width="608" height="416"/>
+</tileset>
