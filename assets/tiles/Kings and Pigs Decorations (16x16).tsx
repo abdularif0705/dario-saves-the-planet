294fa5b235a577283881b2ec9b9aef51e36a4834
Serafim Mazurs <97854030+smazurs@users.noreply.github.com>
2024-03-03 00:48:26 -0500

    Updated tiled assets

--- assets/tiles/Kings and Pigs Decorations (16x16).tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Kings and Pigs Decorations (16x16)" tilewidth="16" tileheight="16" tilecount="168" columns="14">
- <image source="../images/Terrain/Kings and Pigs Decorations (32x32).png" width="224" height="192"/>
-</tileset>
--- assets/tiles/Pirate Bomb Tile-Sets (16x16).tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Pirate Bomb Tile-Sets (16x16)" tilewidth="16" tileheight="16" tilecount="480" columns="24">
- <image source="../images/Terrain/Pirate Bomb Tile-Sets (64-64).png" width="384" height="320"/>
-</tileset>
